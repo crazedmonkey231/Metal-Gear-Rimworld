daa4bb1f0b15f09ccd17b3848a054d7e9077ee85
Michael <crazedmonkey231@users.noreply.github.com>
2022-04-16 18:48:56 -0400

    bionic arm

---
+++ Source/MGRRimworld/MGRApparel/MGRGizmo/MGR_Gizmo_ExoFrame.cs
@@ -0,0 +1,54 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace MGRApparel.MGRGizmo
+{
+    [StaticConstructorOnStartup]
+    class MGR_Gizmo_ExoFrame : Gizmo
+    {
+        public MGR_ExoFrame exoFrame;
+
+        private static readonly Texture2D FullShieldBarTex = SolidColorMaterials.NewSolidColorTexture(new Color(0.2f, 0.2f, 0.24f));
+
+        private static readonly Texture2D EmptyShieldBarTex = SolidColorMaterials.NewSolidColorTexture(Color.clear);
+
+        
+        public MGR_Gizmo_ExoFrame()
+        {
+            base.order = -100f;
+        }
+
+        public override float GetWidth(float maxWidth)
+        {
+            return 140f;
+        }
+
+        public override GizmoResult GizmoOnGUI(Vector2 topLeft, float maxWidth, GizmoRenderParms _params)
+        {
+            Rect overRect = new Rect(topLeft.x, topLeft.y, GetWidth(maxWidth), 75f);
+            Find.WindowStack.ImmediateWindow(984688, overRect, WindowLayer.GameUI, delegate
+            {
+                Rect rect = overRect.AtZero().ContractedBy(6f);
+                Rect rect2 = rect;
+                rect2.height = overRect.height / 2f;
+                Text.Font = GameFont.Tiny;
+                Widgets.Label(rect2, exoFrame.LabelCap);
+                Rect rect3 = rect;
+                rect3.yMin = overRect.height / 2f;
+                float fillPercent = exoFrame.Energy / Mathf.Max(1f, exoFrame.GetStatValue(StatDefOf.EnergyShieldEnergyMax, true));
+                Widgets.FillableBar(rect3, fillPercent, FullShieldBarTex, EmptyShieldBarTex, false);
+                Text.Font = GameFont.Small;
+                Text.Anchor = TextAnchor.MiddleCenter;
+                Widgets.Label(rect3, (exoFrame.Energy * 100f).ToString("F0") + " / " + (exoFrame.GetStatValue(StatDefOf.EnergyShieldEnergyMax, true) * 100f).ToString("F0"));
+                Text.Anchor = TextAnchor.UpperLeft;
+            }, true, false, 1f);
+            return new GizmoResult(GizmoState.Clear);
+        }
+    }
+}
---
+++ Source/MGRRimworld/MGRApparel/MGR_ExoFrame.cs
@@ -0,0 +1,214 @@
+﻿using MGRApparel.MGRGizmo;
+using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace MGRApparel
+{
+    class MGR_ExoFrame : Apparel
+    {
+        private float energy;
+        private int ticksToReset = -1;
+        private int lastKeepDisplayTick = -9999;
+        private Vector3 impactAngleVect;
+        private int lastAbsorbDamageTick = -9999;
+        private const float MinDrawSize = 1.2f;
+        private const float MaxDrawSize = 1.55f;
+        private const float MaxDamagedJitterDist = 0.05f;
+        private const int JitterDurationTicks = 8;
+        private int StartingTicksToReset = 3200;
+        private float EnergyOnReset = 0.2f;
+        private float EnergyLossPerDamage = 0.033f;
+        private int KeepDisplayingTicks = 1000;
+        private float ApparelScorePerEnergyMax = 0.25f;
+
+        //private static readonly Material BubbleMat = MaterialPool.MatFrom("Shields/shield_a", ShaderDatabase.Transparent);
+
+        private float EnergyMax => this.GetStatValue(StatDefOf.EnergyShieldEnergyMax, true);
+
+        private float EnergyGainPerTick => this.GetStatValue(StatDefOf.EnergyShieldRechargeRate, true) / 60f;
+
+        public float Energy => energy;
+
+        public ShieldState ShieldState
+        {
+            get
+            {
+                if (ticksToReset > 0)
+                {
+                    return ShieldState.Resetting;
+                }
+                return ShieldState.Active;
+            }
+        }
+
+        private bool ShouldDisplay
+        {
+            get
+            {
+                Pawn wearer = this.Wearer;
+                if (!wearer.Spawned || wearer.Dead || wearer.Downed)
+                    return false;
+                return wearer.InAggroMentalState || wearer.Drafted || wearer.Faction.HostileTo(Faction.OfPlayer) && !wearer.IsPrisoner || Find.TickManager.TicksGame < this.lastKeepDisplayTick + this.KeepDisplayingTicks;
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref energy, "energy", 0f, false);
+            Scribe_Values.Look(ref ticksToReset, "ticksToReset", -1, false);
+            Scribe_Values.Look(ref lastKeepDisplayTick, "lastKeepDisplayTick", 0, false);
+        }
+
+        public override IEnumerable<Gizmo> GetWornGizmos()
+        {
+            if (Find.Selector.SingleSelectedThing != base.Wearer)
+            {
+                yield break;
+            }
+            yield return (Gizmo)new MGR_Gizmo_ExoFrame
+            {
+                exoFrame = this
+            };
+            ;
+        }
+
+        public override float GetSpecialApparelScoreOffset()
+        {
+            return EnergyMax * ApparelScorePerEnergyMax;
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (base.Wearer == null)
+            {
+                energy = 0f;
+            }
+            else if (ShieldState == ShieldState.Resetting)
+            {
+                ticksToReset--;
+                if (ticksToReset <= 0)
+                {
+                    Reset();
+                }
+            }
+            else if (ShieldState == ShieldState.Active)
+            {
+                energy += EnergyGainPerTick;
+                if (energy > EnergyMax)
+                {
+                    energy = EnergyMax;
+                }
+            }
+        }
+
+        public override bool CheckPreAbsorbDamage(DamageInfo dinfo)
+        {
+            if (ShieldState != 0)
+            {
+                return false;
+            }
+            if (dinfo.Def == DamageDefOf.EMP)
+            {
+                energy = 0f;
+                Break();
+                return false;
+            }
+            if (!dinfo.Def.isRanged && !dinfo.Def.isExplosive)
+            {
+                return false;
+            }
+            energy -= dinfo.Amount * EnergyLossPerDamage;
+            if (energy < 0f)
+            {
+                Break();
+            }
+            else
+            {
+                AbsorbedDamage(dinfo);
+            }
+            return true;
+        }
+
+        public void KeepDisplaying()
+        {
+            lastKeepDisplayTick = Find.TickManager.TicksGame;
+        }
+
+        private void AbsorbedDamage(DamageInfo dinfo)
+        {
+
+
+            SoundDefOf.EnergyShield_AbsorbDamage.PlayOneShot(new TargetInfo(base.Wearer.Position, base.Wearer.Map, false));
+            this.impactAngleVect = Vector3Utility.HorizontalVectorFromAngle(dinfo.Angle);
+            Vector3 loc = base.Wearer.TrueCenter() + this.impactAngleVect.RotatedBy(180f) * 0.5f;
+            float num = Mathf.Min(10f, 2f + dinfo.Amount / 10f);
+            FleckMaker.Static(loc, base.Wearer.Map, FleckDefOf.ExplosionFlash, num);
+            int num2 = (int)num;
+            for (int i = 0; i < num2; i++)
+            {
+                FleckMaker.ThrowDustPuff(loc, base.Wearer.Map, Rand.Range(0.8f, 1.2f));
+            }
+            this.lastAbsorbDamageTick = Find.TickManager.TicksGame;
+            this.KeepDisplaying();
+
+
+        }
+
+        private void Break()
+        {
+            SoundDefOf.EnergyShield_Broken.PlayOneShot(new TargetInfo(base.Wearer.Position, base.Wearer.Map, false));
+            FleckMaker.Static(base.Wearer.TrueCenter(), base.Wearer.Map, FleckDefOf.ExplosionFlash, 12f);
+            for (int i = 0; i < 6; i++)
+            {
+                Vector3 loc = base.Wearer.TrueCenter() + Vector3Utility.HorizontalVectorFromAngle((float)Rand.Range(0, 360)) * Rand.Range(0.3f, 0.6f);
+                FleckMaker.ThrowDustPuff(loc, base.Wearer.Map, Rand.Range(0.8f, 1.2f));
+            }
+            energy = 0f;
+            ticksToReset = StartingTicksToReset;
+        }
+
+        private void Reset()
+        {
+            if (base.Wearer.Spawned)
+            {
+                SoundDefOf.EnergyShield_Reset.PlayOneShot(new TargetInfo(base.Wearer.Position, base.Wearer.Map, false));
+                FleckMaker.ThrowLightningGlow(base.Wearer.TrueCenter(), base.Wearer.Map, 3f);
+            }
+            ticksToReset = -1;
+            energy = EnergyOnReset;
+        }
+
+        public override void DrawWornExtras()
+        {
+/*            if (ShouldDisplay)
+            {
+                float num = Mathf.Lerp(1.2f, 1.55f, energy);
+                Vector3 vector = base.Wearer.Drawer.DrawPos;
+                vector.y = AltitudeLayer.MoteOverhead.AltitudeFor();
+                int num2 = Find.TickManager.TicksGame - lastAbsorbDamageTick;
+                if (num2 < 8)
+                {
+                    float num3 = (float)(8 - num2) / 8f * 0.05f;
+                    vector += impactAngleVect * num3;
+                    num -= num3;
+                }
+                float angle = (float)Rand.Range(0, 360);
+                Vector3 s = new Vector3(num, 1f, num);
+                Matrix4x4 matrix = default(Matrix4x4);
+                matrix.SetTRS(vector, Quaternion.AngleAxis(angle, Vector3.up), s);
+
+                Graphics.DrawMesh(MeshPool.plane10, matrix, BubbleMat, 0);
+            }*/
+        }
+
+        public override bool AllowVerbCast(Verb verb)
+        {
+            return true;
+        }
+    }
+}
